d62184247915a2878d537a847e8a081bb950477c
Innovationtech <hectordehoyos053@gmail.com>
2024-08-16 22:40:42 -0500

    Updates

---
+++ precache-manifest.56e688584fa0bdc21a61db60667b00c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04d68be2059b2aa2e4c97abb88b37b5",
+    "url": "/innovationtech1/smartpests.git/index.html"
+  },
+  {
+    "revision": "ec826ef32771622d1cc4",
+    "url": "/innovationtech1/smartpests.git/static/css/main.359bda72.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/innovationtech1/smartpests.git/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/innovationtech1/smartpests.git/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "ec826ef32771622d1cc4",
+    "url": "/innovationtech1/smartpests.git/static/js/main.9734e1b1.chunk.js"
+  },
+  {
+    "revision": "003ca5345f637d8baee3",
+    "url": "/innovationtech1/smartpests.git/static/js/runtime~main.9f9e5512.js"
+  },
+  {
+    "revision": "67920950391803d46acd45ec44a85fc2",
+    "url": "/innovationtech1/smartpests.git/static/media/logo192.67920950.png"
+  }
+]);
